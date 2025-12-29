f6997fac88c671385020f1be70c353d0c3cd016a
sanjay <sanjaygolappanvar1@gmail.com>
2025-12-29 14:00:27 +0530

    Set up React frontend with basic App structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// Main App component for displaying blog articles
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>BeyondChats Blogs</h1>
+      <p>Frontend is running</p>
+    </div>
+  );
+}
+
+export default App;
